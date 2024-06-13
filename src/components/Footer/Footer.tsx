0e82f200b4e84935e8ae7ae76aec0e68fca5c3e6
learsiOtni <learsiotni@gmail.com>
2024-06-13 01:44:06 +0100

    responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import {
-  faAddressBook,
   faEnvelope,
   faLink,
 } from "@fortawesome/free-solid-svg-icons";
@@ -8,18 +7,18 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="p-2.5 flex justify-between border-t py-10 border-t border-t-primary/20">
-      <div className="max-w-xl">
-        <p className="text-sm">&copy; Izzy Labarinto</p>
-        <p className="mt-2.5    text-body">
+    <div className="flex flex-wrap justify-between border-t py-10 border-t border-t-primary/20">
+      <div className="max-w-xl mt-2.5">
+        <p className="text-[#cfd9ed] text-xs sm:text-sm">&copy; Izzy Labarinto</p>
+        <p className="my-2.5 text-body">
           Loosely designed with pen and paper, and coded in Visual Studio Code.
           Built with Next.js and Tailwind CSS, deployed with Vercel. All text is
           set in the Inter typeface.
         </p>
       </div>
 
-      <div className="mr-48 flex flex-col gap-4 w-[250px]">
-        <h1 className="text-title">Contact Me</h1>
+      <div className="mt-2.5 md:mr-2 lg:mr-48 flex flex-col gap-4 w-[250px]">
+        <h1 className="text-subtitle">Contact Me</h1>
         <p className="links text-body">
           <Link href="https://github.com/learsiOtni" className="group">
             Check out my Github
